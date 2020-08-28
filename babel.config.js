3bc868cacc816a660aa4cd3fb662ba290859c4aa
Gabriel França <dev.gabrielfranca@gmail.com>
2020-08-27 21:53:01 -0300

    feat: adding absulte import

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: ["next/babel"],
   plugins: [
     ["styled-components", { "ssr": true }],
-    "inline-react-svg"
+    "inline-react-svg",
+    [
+      "babel-plugin-root-import", {
+        "rootPathSuffix": "src",
+        "rootPathPrefix": "~"
+      }
+    ]
   ]
 }

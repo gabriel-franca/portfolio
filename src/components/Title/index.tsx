95512bb5a593a9212cf9bba18092cd2c4d390a54
Gabriel França <dev.gabrielfranca@gmail.com>
2020-08-27 21:57:21 -0300

    feat: dev tools (#10)
    * feat: adding eslint
    
    * feat: adding prettier
    
    * feat: adding editorconfig
    
    * refactor: changing file structure
    
    * feat: adding styled-components
    
    * feat: adding handle to images
    
    * feat: adding absulte import

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Title: React.FC = ({ children }) => <h1>{children}</h1>
+
+export default Title
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: {
+      main: '#191A1E'
+    },
+    common: {
+      white: '#ffffff'
+    }
+  }
+}
+
+export default theme

a78f09ab45310b5cbfbd476216898a89905cc713
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-09 21:05:45 -0300

    style: improving _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
 import Document, {
-  DocumentInitialProps,
-  DocumentContext,
   Html,
   Head,
   Main,
-  NextScript
+  NextScript,
+  DocumentContext
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -38,13 +34,10 @@ export default class MyDocument extends Document {
     }
   }
 
-  render(): JSX.Element {
+  render() {
     return (
-      <Html lang="pt">
-        <Head>
-          <meta charSet="utf-8" />
-        </Head>
-
+      <Html lang="pt-BR">
+        <Head />
         <body>
           <Main />
           <NextScript />

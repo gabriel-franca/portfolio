45792dc064fc109ebd64bd5826b05f35434f9c2e
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-09 22:03:08 -0300

    feat: adding global configs (#19)
    * feat: adding more global styles
    
    * feat: adding head configs to site
    
    * fix: fixing bug in svg property
    
    * test: fixing snapshot test to <Main />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
 
 import GlobalStyle from '~/styles/global'
 import theme from '~/styles/theme'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Gabriel França</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="Hey, I'm Gabriel França! Passionate about programming and technology. Always looking to create valuable and well-crafted softwares that can help the world in some way. Working remotely, focused on TypesScript, Node.js, ReactJS, NextJS and React Native. Currently I have been studying ways to quickly and easily write code that is easy to read and maintain, through software architectures, principles and good programming practices. I love to study, learn and share new things."
+        />
+      </Head>
+
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
   )
 }
 
-export default MyApp
+export default App

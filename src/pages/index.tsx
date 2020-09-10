977e0e739d484ad40933114ab095e42f003bc9c1
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-09 21:34:15 -0300

    feat: adding head configs to site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 
 import Logo from '~/assets/logo.svg'
 
@@ -7,10 +6,6 @@ import * as S from '~/styles/pages/home'
 
 const Home: React.FC = () => (
   <S.Container>
-    <Head>
-      <title>Gabriel França</title>
-    </Head>
-
     <Logo />
   </S.Container>
 )

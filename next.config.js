ce42f77fac7ce6525918842ec70025da57d2c238
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-14 22:42:58 -0300

    feat: adding PWA (#22)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withImages = require('next-images')
+const withPWA = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withImages({
   esModule: true
+})
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
 })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <title>Gabriel França</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="Hey, I'm Gabriel França! Passionate about programming and technology. Always looking to create valuable and well-crafted softwares that can help the world in some way. Working remotely, focused on TypesScript, Node.js, ReactJS, NextJS and React Native. Currently I have been studying ways to quickly and easily write code that is easy to read and maintain, through software architectures, principles and good programming practices. I love to study, learn and share new things."

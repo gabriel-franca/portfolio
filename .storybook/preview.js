4798faf13e67696e1260c8bccc8a98fbd1c64afe
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-14 21:38:59 -0300

    feat: adding storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' }
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]

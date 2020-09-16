0c8df9a0651dc464ff5470b2d1d86f92d77476c0
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-15 21:49:00 -0300

    fix: fixing theme import

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,7 +1,7 @@
 /* eslint @typescript-eslint/no-empty-interface: "off" */
 import 'styled-components'
 
-import theme from './theme'
+import theme from '~/styles/theme'
 
 export type Theme = typeof theme
 

1e4f359f38c805353d25ab25a18e3f2e5c68032a
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-21 20:28:06 -0300

    feat: adding Montserrat font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,6 +2,15 @@ import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   ${({ theme }) => css`
+    @font-face {
+      font-family: 'Montserrat';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: local('Montserrat Regular'), local('Montserrat-Regular'),
+        url('../fonts/montserrat-regular.woff2') format('woff2');
+    }
+
     * {
       margin: 0;
       padding: 0;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,18 +9,7 @@ const theme = {
   },
   typography: {
     htmlFontSize: '62.5%',
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      "'Segoe UI'",
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      "'Open Sans'",
-      "'Helvetica Neue'",
-      'sans-serif'
-    ]
+    fontFamily: ['Montserrat', 'sans-serif']
   }
 }
 
